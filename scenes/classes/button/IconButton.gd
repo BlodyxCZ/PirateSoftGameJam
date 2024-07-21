5247006bfa427ba0af0721d808d21f21047ffef0
BlodyxCZ <kjubercz@gmail.com>
2024-07-21 20:53:12 +0200

    More Pause functionality
    Added custom button class that shows an icon next to the selected item

---
+++ scenes/classes/button/IconButton.gd
@@ -0,0 +1,19 @@
+class_name IconButton
+extends Button
+
+
+@onready var _icon = icon
+
+
+func _ready() -> void:
+	focus_entered.connect(_on_focused)
+	focus_exited.connect(_on_unfocused)
+	_on_unfocused()
+
+
+func _on_focused() -> void:
+	icon = _icon
+
+
+func _on_unfocused() -> void:
+	icon = null
--- scenes/overlay/Overlay.gd
+++ scenes/overlay/Overlay.gd
@@ -7,6 +7,11 @@ func _process(delta: float) -> void:
 
 
 func pause() -> void:
+	$Pause/VBoxContainer/Resume.grab_focus()
 	get_tree().current_scene.process_mode = Node.PROCESS_MODE_PAUSABLE
 	get_tree().paused = !get_tree().paused
 	$Pause.visible = get_tree().paused
+
+
+func _on_resume_pressed() -> void:
+	pause()

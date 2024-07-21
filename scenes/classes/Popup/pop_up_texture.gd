3f33e71eba60f710aeb9b6ba8f2169e756e46782
BlodyxCZ <kjubercz@gmail.com>
2024-07-21 20:35:05 +0200

    Added Pause menu fundtionality
    Added functionality for pause menu

--- scenes/classes/Popup/pop_up_texture.gd
+++ scenes/classes/Popup/pop_up_texture.gd
@@ -2,9 +2,8 @@ extends SubViewport
 
 
 func _process(_delta: float) -> void:
-	for child in get_children():
-		if child is Sprite2D:
-			if child.name == Global.controllers.keys()[Global.controller]:
-				child.show()
-			else:
-				child.hide()
+	for child in $Interact.get_children():
+		if child.name == Global.controllers.keys()[Global.controller]:
+			child.show()
+		else:
+			child.hide()
--- scenes/overlay/Overlay.gd
+++ scenes/overlay/Overlay.gd
@@ -1,3 +1,12 @@
 extends CanvasLayer
 
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		pause()
+
+
+func pause() -> void:
+	get_tree().current_scene.process_mode = Node.PROCESS_MODE_PAUSABLE
+	get_tree().paused = !get_tree().paused
+	$Pause.visible = get_tree().paused

d5951b7820260fa8046e97e484a7a27f1e2d885e
BlodyxCZ <kjubercz@gmail.com>
2024-07-24 21:17:00 +0200

    Added Fridge functionality, Updated Oven functionality
    Added functionality to Fridge, player can click button to take an item
    Updated Oven functionality to change item state

--- scenes/classes/ObjectGUI/object_gui.gd
+++ scenes/classes/ObjectGUI/object_gui.gd
@@ -2,6 +2,9 @@ class_name ObjectGUI
 extends Control
 
 
+@onready var player: Player = get_tree().get_first_node_in_group("Player")
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action("interact2") and event.is_pressed() and not event.is_echo() and self.visible:
 		await get_tree().create_timer(0.1).timeout
--- scenes/objects/interactable/oven/GUI.gd
+++ scenes/objects/interactable/oven/GUI.gd
@@ -1,1 +1,8 @@
 extends ObjectGUI
+
+
+const CARROT_ITEM = preload("res://scenes/classes/Item/Items/Carrot/CarrotItem.tres")
+
+
+func _on_carrot_pressed() -> void:
+	player.held_item = CARROT_ITEM

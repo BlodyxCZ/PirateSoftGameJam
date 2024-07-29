7bcf41d59a4b50aeb9ce1d1e0bb19ea072f6c942
BlodyxCZ <kjubercz@gmail.com>
2024-07-29 20:30:59 +0200

    Added Dialog Emotions

--- scenes/overlay/Dialogue.gd
+++ scenes/overlay/Dialogue.gd
@@ -21,31 +21,43 @@ func first_meeting() -> void:
 	Overlay.dialog_finished.emit()
 
 
-func vampire_say(what: String, duration: float) -> void:
-	$Vampire.play()
+func vampire_say(what: String, duration: float, emotion: String = "Normal") -> void:
+	for video: VideoStreamPlayer in $Vampire.get_children():
+		if video.name == emotion:
+			video.show()
+			video.play()
+		else:
+			video.hide()
 	$VampireSpeech.show()
+	
 	$VampireSpeech/VampireText.visible_ratio = 0.0
 	$VampireSpeech/VampireText.text = what
 	var tween: Tween = create_tween()
 	tween.tween_property($VampireSpeech/VampireText, "visible_ratio", 1.0, duration)
 	tween.play()
 	await tween.finished
-	$Vampire.stop()
-	$Vampire.stream_position = 0.0
+	
+	for video: VideoStreamPlayer in $Vampire.get_children():
+		video.stop()
+		video.stream_position = 0.0
+	
 	await get_tree().create_timer(2.0).timeout
 	$VampireSpeech.hide()
 
 
-func jackalope_say(what: String, duration: float) -> void:
+func jackalope_say(what: String, duration: float, emotion: String = "normal") -> void:
 	$Jackalope.play()
 	$JackalopeSpeech.show()
+	
 	$JackalopeSpeech/JackalopeText.visible_ratio = 0.0
 	$JackalopeSpeech/JackalopeText.text = what
 	var tween: Tween = create_tween()
 	tween.tween_property($JackalopeSpeech/JackalopeText, "visible_ratio", 1.0, duration)
 	tween.play()
 	await tween.finished
+	
 	$Jackalope.stop()
 	$Jackalope.stream_position = 0.0
+	
 	await get_tree().create_timer(2.0).timeout
 	$JackalopeSpeech.hide()
